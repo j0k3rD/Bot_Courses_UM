33fb3871ed531ceeb2e3156581b03f0e7e0099b4
Markitos <48894558+XxRaXoRxX@users.noreply.github.com>
2023-02-05 15:10:27 -0300

    fix service/__init__.py error

--- main/services/__init__.py
+++ main/services/__init__.py
@@ -2,4 +2,4 @@ from .scrap_services import ScrapServices
 from .browser_firefox import FirefoxBrowser
 from .browser_chrome import ChromeBrowser
 from .browser import Browser
-from course_services import CourseService
+from .course_services import CourseService
