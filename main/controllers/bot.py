9911902bede0ff98e4b939d457c964410643a958
j0k3r-Desktop <agm.moya@alumno.um.edu.ar>
2023-02-10 14:39:31 -0300

    bot controller refactorized

--- main/controllers/bot.py
+++ main/controllers/bot.py
@@ -22,19 +22,17 @@ class Bot():
             else:
                 embed = self.text_embed_found(keyword)
                 #Convertir el string en una lista
-                x = ast.literal_eval(response.json()['message'])
+                courses = ast.literal_eval(response.json()['message'])
                 # Guardar información del usuario
                 user_id = ctx.message.author.id
                 user_name = ctx.message.author
-                # self.save_user(user_name = user_name, user_id = user_id)
-
+                self.save_user(user_name = user_name, user_id = user_id)
                 # Guardar lo escrito por el usuario
                 self.save_search(keywords = keyword, user_id = ctx.message.author.id)
-
                 # Guardar los cursos en la base de datos
-                self.save_course(courses = x, user_id = user_id)
+                self.save_course(courses = courses, user_id = user_id)
 
-                self.text_embed_found_setter(ctx, embed, x)
+                self.text_embed_found_setter(ctx, embed, courses)
             
             await ctx.send(embed = embed)
 
@@ -48,12 +46,13 @@ class Bot():
             else:
                 embed = self.text_embed_found_top()
                 #Convertir el string en una lista
-                x = ast.literal_eval(response.json()['message'])
-                self.text_embed_found_setter(ctx, embed, x)
+                courses = ast.literal_eval(response.json()['message'])
+                self.text_embed_found_setter(ctx, embed, courses)
             
             await ctx.send(embed = embed)
 
         return Process(target=bot.run, args=(os.getenv('DISCORD_TOKEN'),))
+
 
     def save_user(self, user_name, user_id):
 
@@ -80,6 +79,7 @@ class Bot():
 
         requests.post(url = f"{api_url}searches", json = search_data)
 
+
     def save_course(self, courses, user_id):
 
         api_url = os.getenv('API_URL')
@@ -94,25 +94,34 @@ class Bot():
 
 
     def text_embed_not_found(self, ctx, user_input):
+
         embed = discord.Embed(title = f"{BotConstants.BOT_NOT_FIND_COURSE_0} *{user_input}* {BotConstants.BOT_NOT_FIND_COURSE_1}", description = BotConstants.BOT_MORE_COURSE, color = discord.Color.red())
         embed.set_author(name=ctx.message.author)
         embed.set_footer(text=BotConstants.SET_FOOTER)
         embed.timestamp = datetime.datetime.now()
         return embed
 
+
     def text_embed_not_found_top(self):
+
         embed = discord.Embed(title = f"{BotConstants.BOT_TOP_NOT_COURSE}", description = BotConstants.BOT_MORE_COURSE, color = discord.Color.red())
         return embed
 
+
     def text_embed_found(self, user_input):
+
         embed = discord.Embed(title = f"{BotConstants.BOT_FIND_COURSE_0} *{user_input}* {BotConstants.BOT_FIND_COURSE_1}", description = BotConstants.BOT_MORE_COURSE, color = discord.Color.green())
         return embed
 
+
     def text_embed_found_top(self):
+
         embed = discord.Embed(title = f"{BotConstants.BOT_TOP_COURSE}", description = BotConstants.BOT_MORE_COURSE, color = discord.Color.yellow())
         return embed
 
+
     def text_embed_found_setter(self, ctx, embed, x):
+        
         if len(x) < 21:
             for i in range(len(x)):
                 embed.add_field(name=f"📕 - {x[i][0]}", value=f"{x[i][1]}", inline=False)
